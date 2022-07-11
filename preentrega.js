872f112d5a875aa2e21b2d24a04085eb2854faf0
Damián Di Febo <difebodamian@gmail.com>
2022-07-10 22:34:58 -0300

    proceso

--- preentrega.js
+++ preentrega.js
@@ -1,14 +1,14 @@
 
-    /*Mi proyecto trata de una tienda de videojuegos, donde la idea es que se despliegue un catálogo y se le pregunte
-    al usuario cuál producto desea comprar, el producto que elija va a ser agregado al carrito. 
-    Después de cada vez que se agrega un producto, al usuario se le pregunta si quiere seguir eligiendo productos, 
-    una vez que el usuario no quiera agregar más, se le muestra su carrito.
-    Después de mostrarle el carrito al usuario hay que preguntar si quiere eliminar algún producto de su carrito
-    En el caso de que quiera, le preguntamos cuál quiere eliminar, en el caso de que no quiera finaliza el programa
-    Luego puedo agregar la opción de pagar en cuotas */
-/* Primer paso, crear los objetos y pedir los datos al usuario */
-/* Pedirle al usuario que elija un producto */
-/* Añadir al carrito el producto que el usuario eligió */
+/*Mi proyecto trata de una tienda de videojuegos, donde la idea es que se despliegue un catálogo y se le pregunte
+al usuario cuál producto desea comprar, el producto que elija va a ser agregado al carrito. 
+Después de cada vez que se agrega un producto, al usuario se le pregunta si quiere seguir eligiendo productos, 
+una vez que el usuario no quiera agregar más, se le muestra su carrito.
+Después de mostrarle el carrito al usuario hay que preguntar si quiere eliminar algún producto de su carrito
+En el caso de que quiera, le preguntamos cuál quiere eliminar, en el caso de que no quiera finaliza el programa
+Luego puedo agregar la opción de pagar en cuotas */
+/* Primer paso, crear los objetos y pedir los datos al usuario */ //LISTO
+/* Pedirle al usuario que elija un producto */ //LISTO
+/* Añadir al carrito el producto que el usuario eligió */ //LISTO
 /* Preguntarle al usuario si quiere seguir eligiendo productos */
 /* Si el usuario quiere, volver a mostrar el catálogo y que elija un nuevo producto */
 /* Ese producto agregarlo al carrito y volver a preguntarle al usuario si quiere seguir comprando */
@@ -22,40 +22,61 @@
 /* Mostrar precio final y carrito */
 /* Si no quiere pagar en cuotas mostrar carrrito y precio final */
 
-const catalogo = [
-    {nombre: "resident evil village", genero: "terror", valor: 60},
-    {nombre: "gta v", genero: "acción - mundo abierto", valor: 15},
-    {nombre: "assassin's creed origins", genero: "acción - mundo abierto", valor: 30},
-    {nombre: "the sims 4", genero: "simulación", valor: 5},
-    {nombre: "cyberpunk", genero: "fps - mundo abierto", valor: 70},
-    {nombre: "god of war", genero: "acción", valor: 60},
-    {nombre: "far cry 5", genero: "fps - mundo abierto", valor: 40},
-    {nombre: "fallout 4", genero: "rol - mundo abierto", valor: 25}
-]
-
-
-let carrito;
-
-function mostrarCatalogo() {
-    catalogo.forEach((juego, indice) => console.log(`${indice}: ${juego.nombre}. Valor: $${juego.valor} \n`));
-    alert("En la consola tenés el catálogo")
-} 
-
-function mostrarCarrito() {}
-
-function agregarAlCarrito () {}
-
-function eliminarDelCarrito () {}
-
-function pedirDatosAlUsuario() {
-    const productoIngresado = prompt("Ingrese el juego que desea buscar", mostrarCatalogo());
-    console.log(filtrarProducto(productoIngresado));
+class Videojuego {
+    constructor(nombre, genero, valor) {
+        this.nombre = nombre;
+        this.genero = genero;
+        this.valor = valor;
+    }
 }
 
-function filtrarProducto(productoFilter) {
-    const filtrar = catalogo.filter(elemento => elemento.nombre.indexOf(productoFilter) !== -1);
-    return filtrar
+const juego1 = new Videojuego("resident evil village", "terror", 60);
+const juego2 = new Videojuego("gta v", "terror", 15);
+const juego3 = new Videojuego("assassin's creed origins", "terror", 30);
+const juego4 = new Videojuego("the sims 4", "terror", 15);
+const juego5 = new Videojuego("cyberpunk", "terror", 70);
+const juego6 = new Videojuego("god of war", "terror", 50);
+const juego7 = new Videojuego("far cry 5", "terror", 30);
+const juego8 = new Videojuego("fallout 4", "terror", 30);
+
+const catalogo = [juego1, juego2, juego3, juego4, juego5, juego6, juego7, juego8]
+
+const carrito = []
+let seguir = false;
+
+function mostrarCatalogo() {
+    catalogo.map((juego, valor) => console.log(`${juego.nombre} $${juego.valor}`))
+}
+
+function pedirDatosAlUsuario() {
+    let entrada = prompt("Ingrese el videojuego que desea agregar a su carrito");
+    let filtracion = filtrarJuegos(entrada);
+    return filtracion;
+}
+
+function filtrarJuegos(juegoFilter) {
+    const filtrado = catalogo.filter(element => element.nombre.indexOf(juegoFilter) !== -1)
+    return filtrado
+}
+
+function agregarCarrito(juegoCarrito) {
+    carrito.push(juegoCarrito)
+}
+
+function mostrarCarrito() {
+    console.log(carrito)
+}
+
+function calcularTotalCarrito() {
+    const total = carrito.reduce((acc, juego) => acc += parseInt(juego.valor), 0);
+    console.log(total)
 }
 
 mostrarCatalogo()
-pedirDatosAlUsuario()
+do {
+    agregarCarrito(pedirDatosAlUsuario());
+    seguir = confirm("Quiere seguir comprando?")
+} while (seguir)
+
+mostrarCarrito()
+calcularTotalCarrito()
