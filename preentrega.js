e744909b178b20cdb7a5ae5b69259ed5e7d7dc20
Damián Di Febo <difebodamian@gmail.com>
2022-07-11 19:24:11 -0300

    preentrega_lista

--- preentrega.js
+++ preentrega.js
@@ -9,10 +9,10 @@ Luego puedo agregar la opción de pagar en cuotas */
 /* Primer paso, crear los objetos y pedir los datos al usuario */ //LISTO
 /* Pedirle al usuario que elija un producto */ //LISTO
 /* Añadir al carrito el producto que el usuario eligió */ //LISTO
-/* Preguntarle al usuario si quiere seguir eligiendo productos */
-/* Si el usuario quiere, volver a mostrar el catálogo y que elija un nuevo producto */
-/* Ese producto agregarlo al carrito y volver a preguntarle al usuario si quiere seguir comprando */
-/* Si no quiere seguir comprando, mostrar el carrito */
+/* Preguntarle al usuario si quiere seguir eligiendo productos */ //LISTO
+/* Si el usuario quiere, volver a mostrar el catálogo y que elija un nuevo producto */ //LISTO
+/* Ese producto agregarlo al carrito y volver a preguntarle al usuario si quiere seguir comprando */ //LISTO
+/* Si no quiere seguir comprando, mostrar el carrito */ //LISTO
 /* Preguntarle al usuario si quiere eliminar algún producto de su carrito */
 /* Si quiere, preguntarle cuál quiere eliminar */
 /* Eliminarlo */
@@ -23,60 +23,76 @@ Luego puedo agregar la opción de pagar en cuotas */
 /* Si no quiere pagar en cuotas mostrar carrrito y precio final */
 
 class Videojuego {
-    constructor(nombre, genero, valor) {
+    constructor(id, nombre, genero, valor) {
+        this.id = id;
         this.nombre = nombre;
         this.genero = genero;
         this.valor = valor;
     }
 }
 
-const juego1 = new Videojuego("resident evil village", "terror", 60);
-const juego2 = new Videojuego("gta v", "terror", 15);
-const juego3 = new Videojuego("assassin's creed origins", "terror", 30);
-const juego4 = new Videojuego("the sims 4", "terror", 15);
-const juego5 = new Videojuego("cyberpunk", "terror", 70);
-const juego6 = new Videojuego("god of war", "terror", 50);
-const juego7 = new Videojuego("far cry 5", "terror", 30);
-const juego8 = new Videojuego("fallout 4", "terror", 30);
+const juego1 = new Videojuego(0, "resident evil village", "terror", 60);
+const juego2 = new Videojuego(1, "gta v", "mundo abierto", 15);
+const juego3 = new Videojuego(2, "assassin's creed origins", "mundo abierto", 30);
+const juego4 = new Videojuego(3, "the sims 4", "simulación", 15);
+const juego5 = new Videojuego(4, "cyberpunk", "mundo abierto", 70);
 
-const catalogo = [juego1, juego2, juego3, juego4, juego5, juego6, juego7, juego8]
+const catalogo = [juego1, juego2, juego3, juego4, juego5]
 
-const carrito = []
-let seguir = false;
+const carrito = [];
+let seguirComprando = false;
 
 function mostrarCatalogo() {
-    catalogo.map((juego, valor) => console.log(`${juego.nombre} $${juego.valor}`))
+    catalogo.forEach((element) => console.log(`Id: ${element.id}, ${element.nombre}, $${element.valor}`));
+    alert("En la consola está el catálogo")
 }
 
 function pedirDatosAlUsuario() {
-    let entrada = prompt("Ingrese el videojuego que desea agregar a su carrito");
-    let filtracion = filtrarJuegos(entrada);
-    return filtracion;
+    let entrada = parseInt(prompt("Ingrese el id del videojuego que desea agregar a su carrito"));
+    switch (entrada) {
+        case 0:
+            const salida1 = catalogo.find(element => element.nombre === juego1.nombre);
+            return agregarCarrito(salida1);
+        case 1:
+            const salida2 = catalogo.find(element => element.nombre === juego2.nombre);
+            return agregarCarrito(salida2);
+        case 2:
+            const salida3 = catalogo.find(element => element.nombre === juego3.nombre);
+            return agregarCarrito(salida3);
+        case 3:
+            const salida4 = catalogo.find(element => element.nombre === juego4.nombre);
+            return agregarCarrito(salida4);
+        case 4:
+            const salida5 = catalogo.find(element => element.nombre === juego5.nombre);
+            return agregarCarrito(salida5);
+    }
+
 }
 
-function filtrarJuegos(juegoFilter) {
-    const filtrado = catalogo.filter(element => element.nombre.indexOf(juegoFilter) !== -1)
-    return filtrado
-}
-
-function agregarCarrito(juegoCarrito) {
-    carrito.push(juegoCarrito)
+function agregarCarrito(element) {
+    carrito.push(element)
 }
 
 function mostrarCarrito() {
-    console.log(carrito)
+    console.log(`Su carrito: \n`, carrito)
 }
 
 function calcularTotalCarrito() {
     const total = carrito.reduce((acc, juego) => acc += parseInt(juego.valor), 0);
-    console.log(total)
+    console.log(`El valor total de su carrito es: $${total}`);
 }
 
 mostrarCatalogo()
+
 do {
-    agregarCarrito(pedirDatosAlUsuario());
-    seguir = confirm("Quiere seguir comprando?")
-} while (seguir)
+    pedirDatosAlUsuario();
+    seguirComprando = confirm("Quiere seguir comprando?")
+} while (seguirComprando);
 
 mostrarCarrito()
 calcularTotalCarrito()
+
+
+
+
+
