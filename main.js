31befd06e850172f767761ce4b4c4da71f6d04ee
Damián Di Febo <difebodamian@gmail.com>
2022-07-12 23:33:00 -0300

    DOM_mejorado

--- main.js
+++ main.js
@@ -1,27 +1,56 @@
+class Videojuego {
+    constructor(id, nombre, genero, valor) {
+        this.id = id;
+        this.nombre = nombre;
+        this.genero = genero;
+        this.valor = valor;
+    }
+}
 
+const juego1 = new Videojuego(0, "resident evil village", "terror", 60);
+const juego2 = new Videojuego(1, "gta v", "mundo abierto", 15);
+const juego3 = new Videojuego(2, "assassin's creed origins", "mundo abierto", 30);
+const juego4 = new Videojuego(3, "the sims 4", "simulación", 15);
+
+const cards = document.querySelectorAll(".card");
 const titles = document.querySelectorAll(".title");
-const title_1 = titles[0];
-const title_2 = titles[1];
-const title_3 = titles[2];
-const title_4 = titles[3];
-const cards = document.querySelectorAll(".card")
+const precios = document.querySelectorAll(".precio");
+const generos = document.querySelectorAll(".genero");
+const buttons = document.querySelectorAll(".button")
+const span = document.querySelectorAll("span");
 
-title_2.textContent = "FIFA 22";
-title_3.textContent = "Valorant";
-title_4.textContent = "League of Legends";
+
+titles[0].textContent = juego1.nombre;
+titles[1].textContent = juego2.nombre;
+titles[2].textContent = juego3.nombre;
+titles[3].textContent = juego4.nombre;
+
+precios[0].textContent = "$" + juego1.valor;
+precios[1].textContent = "$" + juego2.valor;
+precios[2].textContent = "$" + juego3.valor;
+precios[3].textContent = "$" + juego4.valor;
+
+generos[0].textContent = juego1.genero;
+generos[1].textContent = juego2.genero;
+generos[2].textContent = juego3.genero;
+generos[3].textContent = juego4.genero;
+
 
 let seguir = false
 
 function seleccionarProducto(num) {
     num -= 1;
-    cards[num].classList.add("border--red")
-    alert("El producto ha sido seleccionado")
+    cards[num].classList.add("border--red");
 }
 
 function pedirDatoUsuario() {
     let entrada = parseInt(prompt("Qué juego desea seleccionar? 1 al 4"));
-    return entrada
+    if (entrada < 5) {
+        return entrada
+    } else {
+        alert("ingrese un valor válido")
+    }
 }
 
 
-seleccionarProducto(pedirDatoUsuario());
+seleccionarProducto(pedirDatoUsuario()); 
